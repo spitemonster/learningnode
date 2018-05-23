1ba08f420a54da3648630832508b64a1fca57f61
Kirby Johnston <info@spitemonster.com>
2018-05-23 14:48:40 -0400

    fldaerr

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send(todos);
+  }, (err) => {
+    console.log(err);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
 });
+
+module.exports = {app};

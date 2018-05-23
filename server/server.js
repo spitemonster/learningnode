f85bcae5123262bff2edc54978349db8c08fe9ff
Kirby Johnston <info@spitemonster.com>
2018-05-23 14:54:41 -0400

    vaef

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send(todos);
+    res.send({todos});
   }, (err) => {
     console.log(err);
   });

d90994010c9e711c1f547314131ec268aac7843e
Kirby Johnston <info@spitemonster.com>
2018-05-25 15:35:33 -0400

    tests in place blafarg

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,38 +1,21 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = '123abc!';
 
-let token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    if (err) {
+      return console.log(err);
+    }
 
-let decoded = jwt.verify(token, '123abc');
-console.log(decoded);
-// jwt.verify
+    console.log(hash);
+  });
+});
 
-// let message = 'I am a string';
-// let hash = SHA256(message).toString();
-//
-// console.log(message, hash);
-//
-// let data = {
-//   id: 4
-// }
-//
-// let token = {
-//   data,
-//   hash: SHA256(JSON.stringify(data)).toString()
-// }
-//
-// token.data.id = 5;
-// token.hash = SHA256(JSON.stringify(token.data)).toString();
-//
-// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// if (resultHash === token.hash) {
-//   console.log('Data was not changed');
-// } else {
-//   console.log('data was changed');
-// }
+let hashedPassword = '$2a$10$tiXamgJVO8p7d.6.b1ER8O7Mr/T5Y9oabKI9.Jw0zo6B84VxzG30m';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});

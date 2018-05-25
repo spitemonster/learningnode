4bad39716a13867501bd11e95588322d6630b0e9
Kirby Johnston <info@spitemonster.com>
2018-05-25 16:05:34 -0400

    vaefd

--- server/server.js
+++ server/server.js
@@ -65,6 +65,20 @@ app.post('/users', (req, res) => {
   })
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+  // res.send(body);
+});
+
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
